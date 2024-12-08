e2d778cf1f7aa216b8e43904b1468d95b05e1739
Cloviski <claudioribeiro.j@hotmail.com>
2024-12-08 16:57:24 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div className="flex h-screen w-screen items-center justify-center">
+      <div className="bg-flight-gray flex h-[100px] w-[960px] items-center justify-center rounded-xl">
+        <input />
+        <input />
+        <input />
+        <input />
+      </div>
+    </div>
+  );
+}
+
+export default App;
